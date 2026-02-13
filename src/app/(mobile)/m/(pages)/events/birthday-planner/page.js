876ebe7fb74675cefe9e3f4a7d0ef84660ff0995
planwab_pvt_ltd <prashant@planwab.com>
2026-02-13 18:52:01 +0530

    Delete src/app/(mobile)/m/(pages)/events/birthday-planner directory

--- src/app/(mobile)/m/(pages)/events/birthday-planner/page.js
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import BirthdayPlannerWrapper from '@/components/mobile/PagesWrapper/BirthdayPlannerWrapper';
-
-export default function BirthdayPlannerPage() {
-  return <BirthdayPlannerWrapper />;
-}
