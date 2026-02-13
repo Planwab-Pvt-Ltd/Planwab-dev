331a7ac011279263e34368f5623d039112fc9d39
OmDeshmukh-01 <deshmukhom330@gmail.com>
2026-02-13 18:41:05 +0530

    added metadata

--- src/app/(mobile)/m/(pages)/events/[category]/birthday-planner/page.js
+++ src/app/(mobile)/m/(pages)/events/[category]/birthday-planner/page.js
@@ -1,5 +1,11 @@
 'use client';
+
 import BirthdayPlannerWrapper from '@/components/mobile/PagesWrapper/BirthdayPlannerWrapper';
+
+export const metadata = {
+  title: "Birthday Planner",
+  description: "Plan the perfect birthday party.",
+};
 
 export default function BirthdayPlannerPage() {
   return <BirthdayPlannerWrapper />;
